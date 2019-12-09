7aa10932981d473749ede4faff37890acafd1ae6
Meliodas <58240192+mimill123456@users.noreply.github.com>
2019-12-09 22:44:24 +0800

    1.2
    1.更新了杆型数据
    2.更新了提钩技能
    3.更新了鱼类传承录
    4.更新了特殊鱼需要的前置天气（如果有的话）
    5.更新了鱼王图标
    6.修复了点击上面渔场时,鱼详情中渔场错误显示的问题

---
+++ 1.2/ff_test1.py
@@ -0,0 +1,82 @@
+# import sys
+# sys.path.append(r"F:\PycharmProjects\Utils")
+import fishinfo
+import json
+
+
+sql="SELECT a.fish_id,a.fish_name,a.time,a.weather,b.area_name,a.is_king,a.tug,a.folklore,a.hookset,a.pweather,a.fish_eyes_skill,a.collect  from ff_fish a LEFT JOIN ff_fisharea b ON a.`where`=b.area_id"
+res=fishinfo.testMysql2(sql)
+js=json.loads(res)
+resx={}
+for x in js:
+    x["baits"]=[]
+    x["areas"]=[]
+    resx[x["fish_name"]]=x
+
+
+sql1 ="SELECT a.fish_id,a.fish_name,a.time,a.weather,b.area_id,b.area_name,c.baitname,c.probability from ff_fish a LEFT JOIN ff_fisharea b ON a.`where`=b.area_id LEFT JOIN ff_bait_fish c ON a.fish_id=c.fish_id"
+
+res1=fishinfo.testMysql2(sql1)
+js1=json.loads(res1)
+for y in js1:
+    resj={}
+    if y["fish_name"]==resx[y["fish_name"]]["fish_name"]:
+        resj["baitname"]=y["baitname"]
+        resj["probability"]=y["probability"]
+        resx[y["fish_name"]]["baits"].append(resj)
+
+
+
+sql2="SELECT a.*,c.fish_name FROM ff_fisharea a LEFT JOIN ff_area_fish b ON a.area_id=b.area_id LEFT JOIN ff_fish c ON b.fish_id=c.fish_id"
+res2=fishinfo.testMysql2(sql2)
+js2=json.loads(res2)
+for f in js2:
+    print(f["fish_name"])
+    if f["fish_name"]:
+        if f["fish_name"]==resx[f["fish_name"]]["fish_name"]:
+            resx[f["fish_name"]]["areas"].append(f["area_name"])
+
+
+
+world_map={}
+big_big_mapkey=set()
+big_map=set()
+fishs={}
+for z in js2:
+    big_big_mapkey.add(z["map_name"])
+    fishs[z["area_id"]] = set()
+
+for zz in big_big_mapkey:
+    world_map[zz]={}
+    for z in js2:
+        for fish in fishs.keys():
+            if z["area_id"] == fish:
+                if z["fish_name"]:
+                    fishs[fish].add(z["fish_name"])
+                else:
+                    fishs[fish].add("不可思议！这里没有鱼！！！！")
+        if z["map_name"]==zz:
+            world_map[zz][z["big_map_name"]]={}
+
+# print(fishs)
+for world in world_map.keys():
+    for big_map in  world_map[world].keys():
+            for z in js2:
+                if z["big_map_name"]==big_map:
+                    world_map[world][big_map][z["area_id"]] = {}
+                    world_map[world][big_map][z["area_id"]]["area_id"] = z["area_id"]
+                    world_map[world][big_map][z["area_id"]]["area_name"] = z["area_name"]
+                    world_map[world][big_map][z["area_id"]]["is_eye"] = z["is_eye"]
+                    world_map[world][big_map][z["area_id"]]["level"] = z["level"]
+                    world_map[world][big_map][z["area_id"]]["fishs"] = []
+                    # world_map[world][big_map].append(area)
+for world in world_map.keys():
+    for big_map in  world_map[world].keys():
+        for area in world_map[world][big_map].keys():
+            world_map[world][big_map][area]["fishs"]=list(fishs[area])
+
+# print(world_map)
+resz={}
+resz["fishinfo"]=resx
+resz["areainfo"]=world_map
+fishinfo.write_to_file("F:/htmls/json2.txt",str(resz))
---
+++ 1.2/readHTML.py
@@ -0,0 +1,19 @@
+import os
+import GitHub_ff14.fishinfo as fishinfo
+
+# testid="440.html"
+#
+# fishinfo.fishinfo(testid)
+# exit()
+file_names = os.listdir("F:/htmls/fish/")
+sqls=[]
+for file in  file_names:
+    sql = fishinfo.fishinfo(file)
+    sqls.append(sql)
+# fishinfo.connecttomysql(sqls)
+
+
+#######测试########
+# select ="SELECT a.fish_id,a.fish_name,a.weather,a.time,c.fish_area_name,b.baitname,b.probability,b.is_small_to_big FROM ff_fish a LEFT JOIN ff_fish_area c ON a.`where`=c.fish_area_id  LEFT JOIN ff_bait_fish b ON a.fish_id=b.fishid "
+# js=fishinfo.testMysql2(select)
+# fishinfo.write_to_file("F:/htmls/json1.txt", str(js))
